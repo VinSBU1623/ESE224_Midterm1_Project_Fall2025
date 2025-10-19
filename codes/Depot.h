cb7c2e4b55d8e370f3cfd0469dd086f5370de538
SelinaC2 <selina.chen.1@stonybrook.edu>
2025-10-19 14:31:30 -0400

    yes!

---
+++ codes/Depot.h
@@ -0,0 +1,14 @@
+#ifndef DEPOT_H
+#define DEPOT_H
+#include <vector>
+#include "Drone.h"
+using namespace std;
+
+class Depot {
+    private:
+        vector<Drone> drones;
+    public:
+        
+
+};
+#endif
